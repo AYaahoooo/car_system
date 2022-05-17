43b65bf095a413d2b428797d3449cb620be1e12b
Ayaahoooo <2070957476@qq.com>
2022-05-17 16:51:18 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { Row,Col } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css' //引入antd样式
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
